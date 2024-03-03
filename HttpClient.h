061e17cccf0c4c1f914c78ac0208f5e2cc94d1a0
K2ot <93932499+K2ot@users.noreply.github.com>
2024-03-03 12:53:30 +0100

    Add files via upload

---
+++ HttpClient.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include <string>
+#include <curl/curl.h>
+
+class HttpClient
+{
+static size_t WriteCallback(void* contents, size_t size, size_t nmemb, void* userp);
+public:
+    HttpClient() {};
+    ~HttpClient() {};
+    std::string make_request(const std::string& url); 
+};
+
+size_t HttpClient::WriteCallback(void* contents, size_t size, size_t nmemb, void* userp)
+{
+    ((std::string*)userp)->append((char*)contents, size * nmemb);
+    return size * nmemb;
+}
+
+std::string HttpClient::make_request(const std::string& url)
+{
+    CURL* curl;
+    CURLcode res;
+    std::string readBuffer;
+
+    curl_global_init(CURL_GLOBAL_DEFAULT);
+    curl = curl_easy_init();
+    if (curl) {
+        curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+        curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
+        curl_easy_setopt(curl, CURLOPT_WRITEDATA, &readBuffer);
+        res = curl_easy_perform(curl);
+
+        if (res != CURLE_OK)
+            fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
+        curl_easy_cleanup(curl);
+    }
+    curl_global_cleanup();
+    return readBuffer;
+}
